67ed5e7a8bf8c8ec577fb1a1ce77747a15ea4f90
yactouat-play <yactouat@hotmail.com>
2023-06-07 12:23:28 +0200

    starting over with succeeding test

--- xml_parser/xml_parser_test.cc
+++ xml_parser/xml_parser_test.cc
@@ -1,21 +1,9 @@
 #include <gtest/gtest.h>
-#include "xml_parser.h"
 
-TEST(XMLTest, CanParseNodeId) {
-    // Arrange
-    // the syntax `std::string xml = R"(...)"` is used to create a raw string literal, 
-    // which is useful when you want to include special characters in a string without needing to escape them
-    std::string xmlStr = R"(
-        <UAVariable NodeId="ns=1;i=20" BrowseName="TestNode" SomeField="some_field">
-            <DisplayName>TestNode</DisplayName>
-            <Description>A test node that you need to parse in cpp</Description>
-        </UAVariable>
-    )";
-    xml_parser::XMLParser parser;
-
-    // Act
-    parser.parse(xmlStr);
-
-    // Assert
-    ASSERT_EQ(parser.getNodeId(), "ns=1;i=20");
+// Demonstrate some basic assertions.
+TEST(XmlParserTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }
